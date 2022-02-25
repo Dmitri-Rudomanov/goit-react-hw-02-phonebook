6f9c189c79854cd0df8fcf016cae67088a16a029
Dmitri-Rudomanov <rudomanovdmitrij@gmail.com>
2022-02-25 15:46:06 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,60 +1,117 @@
 import React, { Component } from 'react';
-import Statistics from './components/Statistics/Statistics';
-import Section from './components/Section/Section';
-import FeedbackOptions from './components/FeedbackOptions/FeedbackOptions';
-import Notification from './components/Notification/Notification';
-
+import shortid from 'shortid';
+// import ColorPicker from './components/ColorPicker';
+// import Counter from './components/Counter';
+import Container from './components/Container';
+import TodoList from './components/TodoList';
+import TodoEditor from './components/TodoEditor';
+import Filter from './components/Filter';
+// import Form from './components/Form';
+import initialTodos from './todos.json';
 
 class App extends Component {
   state = {
-    good: 0,
-    neutral: 0,
-    bad: 0
+    todos: initialTodos,
+    filter: '',
   };
 
-  onLeaveFeedback=(e)=>{
-    const name=e.target.name
-    this.setState((prevState)=>({
-      [name]:prevState[name]+1
-      }))
-  }
+  addTodo = text => {
+    const todo = {
+      id: shortid.generate(),
+      text,
+      completed: false,
+    };
 
-  countTotalFeedback = () => { 
-    const { good, neutral, bad } = this.state
-    const totalCount = good + neutral + bad
-    return totalCount
-  }
+    this.setState(prevState => ({ 
+      todos:[todo,...prevState.todos]
+    }))
+  };
 
-  countPositiveFeedbackPercentage = () => { 
-    const count=Math.round((this.state.good/this.countTotalFeedback())*100)
-    const countCheck=isNaN(count)?"0":count
-    return countCheck
-  }
+  deleteTodo = todoId => {
+    this.setState(prevState => ({
+      todos: prevState.todos.filter(todo => todo.id !== todoId),
+    }));
+  };
 
+  toggleCompleted = todoId => {
+    // this.setState(prevState => ({
+    //   todos: prevState.todos.map(todo => {
+    //     if (todo.id === todoId) {
+    //       return {
+    //         ...todo,
+    //         completed: !todo.completed,
+    //       };
+    //     }
+
+    //     return todo;
+    //   }),
+    // }));
+
+    this.setState(({ todos }) => ({
+      todos: todos.map(todo =>
+        todo.id === todoId ? { ...todo, completed: !todo.completed } : todo,
+      ),
+    }));
+  };
+
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  getVisibleTodos = () => {
+    const { filter, todos } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+
+    return todos.filter(todo =>
+      todo.text.toLowerCase().includes(normalizedFilter),
+    );
+  };
+
+  calculateCompletedTodos = () => {
+    const { todos } = this.state;
+
+    return todos.reduce(
+      (total, todo) => (todo.completed ? total + 1 : total),
+      0,
+    );
+  };
 
   render() {
-    const options=Object.keys(this.state)
+    const { todos, filter } = this.state;
+    const totalTodoCount = todos.length;
+    const completedTodoCount = this.calculateCompletedTodos();
+    const visibleTodos = this.getVisibleTodos();
 
     return (
-      <div>
-        <Section title="Please leave feedback">
-        <FeedbackOptions options={options} onLeaveFeedback={this.onLeaveFeedback}/>
-        </Section>
-        
-        {this.countTotalFeedback()===0
-        ?<Notification message="No feedback given"/>
-        :<Section title="Statistics">
-        <Statistics
-         good={this.state.good}
-          neutral={this.state.neutral}
-          bad={this.state.bad}
-          total={this.countTotalFeedback()}
-          positivePrecentage={this.countPositiveFeedbackPercentage()}/>
-        </Section>}
-      </div>
-    )
+      <Container>
+        {/* TODO: вынести в отдельный компонент */}
 
+        <div>
+          <p>Всего заметок: {totalTodoCount}</p>
+          <p>Выполнено: {completedTodoCount}</p>
+        </div>
+
+        <TodoEditor onSubmit={this.addTodo} />
+
+        <Filter value={filter} onChange={this.changeFilter} />
+
+        <TodoList
+          todos={visibleTodos}
+          onDeleteTodo={this.deleteTodo}
+          onToggleCompleted={this.toggleCompleted}
+        />
+      </Container>
+    );
   }
 }
 
 export default App;
+
+// const colorPickerOptions = [
+//   { label: 'red', color: '#F44336' },
+//   { label: 'green', color: '#4CAF50' },
+//   { label: 'blue', color: '#2196F3' },
+//   { label: 'grey', color: '#607D8B' },
+//   { label: 'pink', color: '#E91E63' },
+//   { label: 'indigo', color: '#3F51B5' },
+// ];
