526c33b90b5b07b8d942e18f1da7ae8ad370cf86
Dmitri-Rudomanov <rudomanovdmitrij@gmail.com>
2022-02-25 17:14:40 +0200

    handleChange

--- src/App.js
+++ src/App.js
@@ -11,9 +11,17 @@ class App extends Component {
     ],
     filter: '',
     name: '',
-    number: ''
+    number: '',
+  }
+
+  handleChange=(e)=>{
+    this.setState({ [e.currentTarget.name]: e.currentTarget.value });
   }
   
+  addNewContact=()=>{
+
+  }
+
   render(){
     return(
       <div>
@@ -24,6 +32,8 @@ class App extends Component {
             <input
               type="text"
               name="name"
+              value={this.state.name}
+              onChange={this.handleChange}
               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
               required
@@ -34,6 +44,8 @@ class App extends Component {
             <input
               type="tel"
               name="number"
+              value={this.state.number}
+              onChange={this.handleChange}
               pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
               title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
               required
